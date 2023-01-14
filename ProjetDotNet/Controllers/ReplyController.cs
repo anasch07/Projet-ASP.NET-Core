1e4bad782d2a88372b84df7717b85316dc14209f
Anas Chaibi <anas.chaibi2@gmail.com>
2023-01-14 13:10:38 +0100

    feat : accept refuse reply + navbar check if user is connected

--- ProjetDotNet/Controllers/ReplyController.cs
+++ ProjetDotNet/Controllers/ReplyController.cs
@@ -8,6 +8,26 @@ namespace ProjetDotNet.Controllers
     [Route("reply")]
     public class ReplyController : Controller
     {
+
+        [Route ("acceptReply")]
+        public  IActionResult acceptReply(int postId, int replyId)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork(AppDbContext.Instance);
+            unitOfWork.Replies.AcceptReply(replyId);
+            unitOfWork.Complete();
+            return RedirectToAction("Index", "Post", new { id = postId });
+        }
+        
+        [Route ("refuseReply")]
+        public  IActionResult refuseReply(int postId, int replyId)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork(AppDbContext.Instance);
+            unitOfWork.Replies.refuseReply(replyId);
+            unitOfWork.Complete();
+            return RedirectToAction("Index", "Post", new { id = postId });
+        }
+        
         
     }
+    
 }
